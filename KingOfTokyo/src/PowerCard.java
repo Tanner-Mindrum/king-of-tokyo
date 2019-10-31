ec745c5a9d357ccf7c6c6df32173928281926a29
Tanner-Mindrum <track2gamesofficial@gmail.com>
2019-10-31 15:19:41 -0700

    initial code commit

---
+++ KingOfTokyo/src/PowerCard.java
@@ -0,0 +1,30 @@
+public class PowerCard {
+
+    private String powerCardName;
+    private int powerCardCost;
+    private String powerCardType;
+
+    public String getPowerCardName() {
+        return this.powerCardName;
+    }
+
+    public void setPowerCardName(String name) {
+        this.powerCardName = name;
+    }
+
+    public int getPowerCardCost() {
+        return this.powerCardCost;
+    }
+
+    public void setPowerCardCost(int cost) {
+        this.powerCardCost = cost;
+    }
+
+    public String getPowerCardType() {
+        return this.powerCardType;
+    }
+
+    public void setPowerCardType(String type) {
+        this.powerCardType = type;
+    }
+}
