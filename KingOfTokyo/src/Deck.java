fdab61e3b5ed500b12164f80340cfdb25e4881d9
Tanner-Mindrum <track2gamesofficial@gmail.com>
2019-11-19 23:12:37 -0800

    Added card list csv, deck class, power card class

---
+++ KingOfTokyo/src/Deck.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Deck {
+
+    private String csvPath;
+    private String line;
+    private String csvSplitter;
+    private ArrayList<PowerCard> deck;  // Deck of Power Cards
+
+    // Default constructor initializes instance variables
+    public Deck() {
+        csvPath = "C:/CECS 343/343-KingOfTokyo-HTT/KingOfTokyo/src/cardList.csv";
+        line = "";
+        csvSplitter = ",";
+        deck = new ArrayList<>();
+    }
+
+    // Creates a deck of Power Cards by iterating through a .csv file, parsing the file's column values,
+    // initializing a Power Card, and adding that Power Card to the deck
+    public void makeDeck() {
+        try (BufferedReader br = new BufferedReader(new FileReader(csvPath))) {  // Read in csv content
+            while ((line = br.readLine()) != null) {  // While a line of content exists in csv
+
+                String[] card = line.split(csvSplitter);  // Split values by comma into [name, cost, type, ability]
+
+                deck.add(new PowerCard(card[0], Integer.parseInt(card[1]), card[2], card[3]));  // Add card to deck
+            }
+        }
+        catch (IOException e) {  // For any strange csv errors
+            e.printStackTrace();
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(deck);
+    }
+
+    // Returns a PowerCard by popping the top one from the deck
+    public PowerCard dealPowerCard() {
+        return deck.remove(0);
+    }
+
+    public void displayDeck() {
+        int displayCount = 1;
+        for (int i = 0; i < deck.size(); i++) {
+            System.out.println("Power Card " + displayCount + ": " + deck.get(i).getPowerCardName());
+            displayCount++;
+        }
+    }
+}
--- KingOfTokyo/src/PowerCard.java
+++ KingOfTokyo/src/PowerCard.java
@@ -3,6 +3,18 @@ public class PowerCard {
     private String powerCardName;
     private int powerCardCost;
     private String powerCardType;
+    private String powerCardAbility;
+
+    // Overloaded constructor initializes instance variables
+    public PowerCard(String powerCardName, int powerCardCost, String powerCardType, String powerCardAbility) {
+        this.powerCardName = powerCardName;
+        this.powerCardCost = powerCardCost;
+        this.powerCardType = powerCardType;
+        this.powerCardAbility = powerCardAbility;
+    }
+
+    /* This is a basic class composed solely of setters and getters for each of the attributes of a Power Card:
+     Name, Cost, Type, Ability */
 
     public String getPowerCardName() {
         return this.powerCardName;
@@ -26,5 +38,13 @@ public class PowerCard {
 
     public void setPowerCardType(String type) {
         this.powerCardType = type;
+    }
+
+    private String getPowerCardAbility() {
+        return this.powerCardAbility;
+    }
+
+    private void setPowerCardAbility(String ability) {
+        this.powerCardAbility = ability;
     }
 }
