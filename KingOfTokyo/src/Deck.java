636794dc09808d1a130d45666c2043ae40a91b98
Tanner-Mindrum <track2gamesofficial@gmail.com>
2019-11-29 15:52:15 -0800

    fixed csv comma issue

--- KingOfTokyo/src/Deck.java
+++ KingOfTokyo/src/Deck.java
@@ -12,7 +12,7 @@ public class Deck {
     public Deck() {
         // Tanner's path: "C:/CECS 343/343-KingOfTokyo-HTT/KingOfTokyo/src/cardList.csv"
         // Tymee's path: "C:/Users/tymee/IdeaProjects/king-of-tokyo/KingOfTokyo/src/cardList.csv"
-        csvPath = "C:/Users/tymee/IdeaProjects/king-of-tokyo/KingOfTokyo/src/cardList.csv";
+        csvPath = "C:/CECS 343/343-KingOfTokyo-HTT/KingOfTokyo/src/cardList.csv";
         line = "";
         csvSplitter = ",";
         deck = new ArrayList<>();
@@ -25,6 +25,23 @@ public class Deck {
             while ((line = br.readLine()) != null) {  // While a line of content exists in csv
 
                 String[] card = line.split(csvSplitter);  // Split values by comma into [name, cost, type, ability]
+
+                //Freeze Time, It has, Regeneration, Monster Sidekick, Thunderstomp
+                if (card[0].equals("It Has a Child")) {
+                    card[3] = "If you are eliminated discard all your cards and lose all your [Star], Heal to 10[Heart] and start again.";
+                }
+                else if (card[0].equals("Freeze Time")) {
+                    card[3] = "On a turn where you score [1][1][1], you can take another turn with one less die.";
+                }
+                else if (card[0].equals("Regeneration")) {
+                    card[3] = "When you heal, heal 1 extra damage.";
+                }
+                else if (card[0].equals("Monster Sidekick")) {
+                    card[3] = "If someone kills you, Go back to 10[Heart] and lose all your [Star]. If either of you or your killer win, or all other players are eliminated then you both win. If your killer is eliminated then you are also. If you are eliminated a second time this card has no effect.";
+                }
+                else if (card[0].equals("Thunder Stomp")) {
+                    card[3] = "\tIf you score 4[Star] in a turn, all players roll one less die until your next turn.";
+                }
 
                 deck.add(new PowerCard(card[0], Integer.parseInt(card[1]), card[2], card[3]));  // Add card to deck
             }
